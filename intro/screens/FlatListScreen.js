a5d95901975352d5b93550b3765c828720d23113
Leonel Molina <satelitehospital@gmail.com>
2025-10-28 20:29:31 -0600

    Practica pendiente

--- intro/screens/FlatListScreen.js
+++ intro/screens/FlatListScreen.js
@@ -1,12 +1,50 @@
-import { Text, StyleSheet, View } from 'react-native'
-
+import { Text, StyleSheet, View, SectionList, ScrollView } from 'react-native'
+import React,{component} from 'react'
 
 export default function FlatListScreen() {
-  
+
+  const datos = [
+    { id: '1', nombre: 'manzana',},
+    { id: '2', nombre: 'platano',},
+    { id: '3', nombre: 'naranja',},
+    { id: '4', nombre: 'uva',},
+    { id: '5', nombre: 'fresa',},
+    { id: '6', nombre: 'Sandia',},
+  ]
+  const secciones = [
+    { title: 'Frutas', data: [
+      {nombre: 'manzana' },
+      {nombre: 'platano' },
+      {nombre: 'naranja' }, 
+      {nombre: 'uva' },
+
+     ] 
+    },
+    
+
+    {
+      titulo: 'Verduras',
+      data: [
+        {nombre: 'zanahoria' },
+        {nombre: 'lechuga' },
+        {nombre: 'tomate' }, 
+        {nombre: 'brocoli' },
+      ]
+    },
+
+
+  ]
     return (
-      <View>
-        <Text>Proximamente</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <View style={styles.content}>
+          <Text style={styles.titulo}>Ejemplo de Flatlist</Text>
+          <FlatList
+
+          
+          
+          > </FlatList>
+        </View>
+        </ScrollView>
     )
 }
 
